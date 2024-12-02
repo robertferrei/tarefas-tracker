3f35fad2c6cd5fec881a497db91aced36ced3a1d
robertferrei <ferreirarobert727@gmail.com>
2024-12-02 07:29:26 -0300

    emitindo novos eventos

--- src/components/FormularioCadastro.vue
+++ src/components/FormularioCadastro.vue
@@ -2,26 +2,10 @@
     <div class="box">
         <div class="columns">
             <div class="column is-8" role="form" aria-label="Formulario para criação de uma nova tarefa">
-                <input type="text" class="input" placeholder=" Qual tarefa voce deseja iniciar ?">
+                <input v-model="descricao" type="text" class="input" placeholder=" Qual tarefa voce deseja iniciar ?">
             </div>
-
             <div class="column">
-                <div class="is-flex is-align-items-center is-justify-content-space-between">
-                    <CronometroRelogio :tempoEmSegundos="tempoEmSegundos"/>
-                    <button class="button" @click="iniciar">
-                        <span class="icon">
-                            <i class="fas fa-play"></i>
-                        </span>
-                        <span>play</span>
-                    </button>
-                    <button class="button" @click="finalizar">
-                        <span class="icon">
-                            <i class="fas fa-stop"></i>
-                        </span>
-                        <span>stop</span>
-                    </button>
-
-                </div>
+                <TemporizadorTempo @ao-temporizador-finalizado="finalizarTarefa"/>               
             </div>
 
         </div>
@@ -30,31 +14,25 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import CronometroRelogio from './CronometroRelogio.vue';
+import TemporizadorTempo from './TemporizadorTempo.vue';
 
 export default defineComponent({
     name: 'FormularioCadastro',
-    components:{ CronometroRelogio },
-
-    data() {
-        return {
-            tempoEmSegundos: 0,
-            cronometro:0,
+    emits: ['aoSalvarTarefa'],  //emitindo evento no APP.VUE
+    components:{ TemporizadorTempo },
+    data(){
+        return{
+            descricao: '',
         }
     },
-  
-
-    methods: {
-        //iniciando o cronometro
-        iniciar() {
-            this.cronometro = setInterval(() => {
-                this.tempoEmSegundos += 1
-            }, 1000)            
+    methods:{
+        finalizarTarefa(tempoDecorrido: number) : void {
+            this.$emit('aoSalvarTarefa',{
+            duracaoEmSegundos: tempoDecorrido,
+            descricao: this.descricao
+            })
+            this.descricao = ''
         },
-        //finalizando  o cronometro 
-        finalizar() {
-            clearInterval(this.cronometro)                          
-        }
     }
 })
 </script>
