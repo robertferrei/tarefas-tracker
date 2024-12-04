88200250df419509a06d583369b8de9c3fc93aeb
robertferrei <ferreirarobert727@gmail.com>
2024-12-04 08:57:27 -0300

    adicionando variavel de css

--- src/components/FormularioCadastro.vue
+++ src/components/FormularioCadastro.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="box">
+    <div class="box formulario">
         <div class="columns">
             <div class="column is-8" role="form" aria-label="Formulario para criação de uma nova tarefa">
                 <input v-model="descricao" type="text" class="input" placeholder=" Qual tarefa voce deseja iniciar ?">
@@ -36,3 +36,10 @@ export default defineComponent({
     }
 })
 </script>
+
+<style >
+.formulario{
+    color:var(--texto-primario);
+    background-color:var(--bg-primario);
+}
+</style>
